38e05d8b1cae9824b7b2172079b08f9ffc3bbd85
@mma423Nanna <alekhya423p@gmail.com>
2021-09-08 09:07:48 -0700

    initialcommit

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,24 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+  const MNEMONIC = 'YOUR WALLET KEY';
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  
   networks: {
     development: {
       host: "127.0.0.1",
       port: "7545",
       network_id: "*" // match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/e1c1766ff1094be687df3b73baefaf27")
+      },
+      network_id: 3,
+      gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
     }
+
   },
   // Configure your compilers
   compilers: {
